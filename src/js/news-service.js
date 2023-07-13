597006fe89446f09a5f3fc9f642bf266c891fee0
EduardMLT <eddku@i.ua>
2023-07-13 13:04:37 +0300

    initial

---
+++ src/js/news-service.js
@@ -0,0 +1,51 @@
+const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+const BASE_URL = 'https://newsapi.org/v2';
+
+// const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+// const BASE_URL = 'https://newsapi.org/v2';
+
+// https://pixabay.com/api/?key=38232376-4840eb4d2a32943b9bc00372c&q=yellow+flowers&image_type=photo
+
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
+ class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=4&page=${this.page}`;
+
+    
+
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(({ articles }) => {
+        this.incrementPage();
+        return articles;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
+export default NewsApiService;
